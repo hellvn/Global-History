30c4f308430d6c66f45d359666c23fb9477391a9
nhat51 <65001571+nhat51@users.noreply.github.com>
2021-08-25 20:48:36 +0700

    nhat

---
+++ GlobalHistory/GlobalHistory/Models/PostTag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace GlobalHistory.Models
+{
+    public partial class PostTag
+    {
+        public int? Id { get; set; }
+        public int? PostId { get; set; }
+        public int? TagId { get; set; }
+
+        public virtual Post Post { get; set; }
+        public virtual Tag PostNavigation { get; set; }
+    }
+}
